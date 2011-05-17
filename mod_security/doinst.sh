a8cef2456ca994a581e6438b81f6670fde6d3327
Papa Issa DIAKHATE <paissad@gmail.com>
2011-05-17 09:17:02 +0000

    The SlackBuild of mod_security was absolutely not correct & was buggy ! (it is corrected now) New README files for some SlackBuilds Improved some other SlackBuilds.
    git-svn-id: https://svn.paissad.net/slackbuilds/stable@24 a8e915ae-7a13-4794-ad7b-daf03aeeedbb

--- mod_security/doinst.sh
+++ mod_security/doinst.sh
@@ -11,21 +11,4 @@ config() {
     # Otherwise, we leave the .new copy for the admin to consider...
 }
 
-CONF_LIST_NEW="etc/mlogc.conf.new \
-etc/rules-updater-example.conf.new \
-etc/rules-updater.conf.new \
-etc/httpd/extra/mod_security.conf.new"
 
-### config stuffs ####
-for conf_new in $CONF_LIST_NEW; do
-    config $conf_new;
-done
-for conf_new in etc/httpd/modsecurity.d/*.new; do
-    config $conf_new
-done
-for conf_new in etc/httpd/modsecurity.d/base_rules/*.new; do
-    config $conf_new
-done
-for conf_new in etc/httpd/modsecurity.d/optional_rules/*.new; do
-    config $conf_new
-done
